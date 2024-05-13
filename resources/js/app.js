cdf8624ee7634597466f592730a9fe73530b2e65
Ilvondir <komsa.m@o2.pl>
2024-05-13 14:00:34 +0200

    inertia vue project configured

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
-import './bootstrap';
+import {createApp, h} from 'vue'
+import {createInertiaApp, Head, Link} from '@inertiajs/vue3'
+
+createInertiaApp({
+    resolve: name => {
+        const pages = import.meta.glob('./pages/**/*.vue', {eager: true})
+        return pages[`./pages/${name}.vue`]
+    },
+    setup({el, App, props, plugin}) {
+        createApp({render: () => h(App, props)})
+            .use(plugin)
+            .component('inertia-head', Head)
+            .component('inertia-link', Link)
+            .mount(el);
+    },
+});
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome');
+});
+
+Route::get("/dupa", function () {
+    return Inertia::render("Dupa");
 });
